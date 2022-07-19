80c744b062dc780b2e867994711343e6b0cba276
dinuh <dinuh@DESKTOP-E2K6OO4>
2022-07-19 11:45:26 +0530

    create git

---
+++ selenium_new/src/test/java/selenium_new/PracticeTest.java
@@ -0,0 +1,18 @@
+package selenium_new;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest
+{
+	@Test
+	public void demo() 
+	{
+		System.out.println("Test1-->Test3");	
+	}
+	@Test
+	public void demo1()
+	{
+		System.out.println("Test2-->Test3");
+	}
+
+}
